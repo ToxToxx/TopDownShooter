d2bdbc527d09feb5a74edd008399e04d7a06da9c
Matvey <sinema3d00@gmail.com>
2024-07-21 14:25:13 +0300

    made bullet component

---
+++ Assets/Scripts/Weapons/BulletComponents/Bullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed = 20f;
+    [SerializeField] private float _range = 25f;
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float distance = _speed * Time.deltaTime;
+        transform.Translate(Vector3.forward * distance);
+
+        if (Vector3.Distance(_startPosition, transform.position) >= _range)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Логика нанесения урона
+        Debug.Log($"{other.name} получил {ShootController.Instance.GetWeaponDamage()} урона");
+        Destroy(gameObject);
+    }
+}
